9f540c6075b6dc7a9429945dea487dcf98905d35
Kindon-Smith <kindon@bluelim3.com>
2025-08-15 15:56:31 -0700

    Updated Match.cs to track data returned by Riot API for MatchDetailsDto

--- Db/objects/Match.cs
+++ Db/objects/Match.cs
@@ -3,10 +3,20 @@ namespace LoLSmith.Db;
 public class Match
 {
     public int Id { get; set; }
-
     public string? MatchId { get; set; }
-
+    
+    // From InfoDto
+    public DateTime GameCreation { get; set; }
+    public long GameDuration { get; set; }
+    public string? GameMode { get; set; }
+    public string? GameType { get; set; }
+    public string? GameVersion { get; set; }
+    public int MapId { get; set; }
+    public string? PlatformId { get; set; }
+    public int QueueId { get; set; }
+    
+    // Navigation properties
     public ICollection<UserMatches> UserMatches { get; set; } = new List<UserMatches>();
-
+    
     public DateTime InsertedAt { get; set; } = DateTime.UtcNow;
 }
