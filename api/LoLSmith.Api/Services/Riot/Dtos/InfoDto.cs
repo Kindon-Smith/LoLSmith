14043d37e61cbab66c8c0cb327f8a0fdead234ed
KindonSmith <kindon@bluelim3.com>
2025-08-17 16:09:49 -0700

    Updated InfoDto.cs so several fields are indicated as nullable

--- api/LoLSmith.Api/Services/Riot/Dtos/InfoDto.cs
+++ api/LoLSmith.Api/Services/Riot/Dtos/InfoDto.cs
@@ -5,10 +5,10 @@ public class InfoDto
     public long GameCreation { get; set; }
     public long GameDuration { get; set; }
     public long GameId { get; set; }
-    public string GameMode { get; set; }
-    public string GameType { get; set; }
-    public string GameVersion { get; set; }
+    public string? GameMode { get; set; }
+    public string? GameType { get; set; }
+    public string? GameVersion { get; set; }
     public int MapId { get; set; }
-    public string PlatformId { get; set; }
+    public string? PlatformId { get; set; }
     public int QueueId { get; set; }
 }
