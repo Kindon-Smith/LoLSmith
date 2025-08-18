a85bc03f31ff7fe9337d323e47f0d1b32e1271e2
Kindon-Smith <kindon@bluelim3.com>
2025-08-18 13:38:45 -0700

    Added rate limiting return values to options config

--- api/LoLSmith.Api/Options/RiotOptions.cs
+++ api/LoLSmith.Api/Options/RiotOptions.cs
@@ -6,4 +6,12 @@ namespace Options.RiotOptions;
 public class RiotOptions
 {
     [Required] public string? ApiKey { get; set; }
+    [Required] public RateLimitOptions RateLimit { get; set; } = new RateLimitOptions();
+
+    public class RateLimitOptions
+    {
+        public bool Enabled { get; set; } = true;
+        public int MaxRequestsPerMinute { get; set; } = 0;
+
+    }
 }
